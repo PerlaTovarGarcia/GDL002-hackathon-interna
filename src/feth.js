959e593b56138fd6ec0df9db347adc8d1387571e
s5tefany <m.garciac11@gmail.com>
2019-03-14 14:42:44 -0600

    se arreglo identacion, se elimino frida y el 5 elemento del catalogo

--- src/feth.js
+++ src/feth.js
@@ -1,20 +1,20 @@
-const arrayGame = ['tt0451279', 'tt0119116', 'tt0117008', 'tt1454029', 'tt0120804',  'tt0120762', 'tt1392190', 'tt0114558', 'tt0780521', 'tt1217209', 'tt0078748', 'tt0089276', 'tt0266697',  'tt1536537', 'tt3077214', 'tt5083738', 'tt0085267', 'tt4925292', 'tt3716530', 'tt0457430', 'tt2873282', 'tt0120679', 'tt1620680'];
-const peliculas =[];
+const arrayMoviesSelected = ['tt0451279', 'tt0117008', 'tt1454029', 'tt0120804', 'tt0120762', 'tt1392190', 'tt0114558', 'tt0780521', 'tt1217209', 'tt0078748', 'tt0089276', 'tt0266697', 'tt1536537', 'tt3077214', 'tt5083738', 'tt0085267', 'tt4925292', 'tt3716530', 'tt0457430', 'tt2873282', 'tt1620680'];
+const movies = [];
 const containerRoot = document.getElementById('img');
 
 
-function card(data) {
+const card = (data) => {
   return (`<div class=" containingImg card">
          <div class="image">
               <img src="${data.Poster}"/>
-              </div>
+         </div>
           <div class="details">
           <div class="center">
               <h1> ${data.Year}</h1>
               <h2> ${data.Title}</h2>
               <h3> ${data.Rated}</h3>
               <h3> ${data.Director}</h3>
-              <h4> ${data.Genre}</h4>
+              <h3> ${data.Genre}</h3>
               <p> ${data.Plot}</p>
         </div>
         </div>
@@ -22,26 +22,24 @@ function card(data) {
 
 };
 
-function showPeliculas (data){
-   let result = "";
-   //containerRoot.innerHTML = '';
-
-     result = containerRoot.innerHTML += card(data);
-       console.log(result);
-   return result;
+const showmovies = (data) => {
+  let result = "";
+  result = containerRoot.innerHTML += card(data);
+  // console.log(result);
+  return result;
 
 }
 
 
 
-for (let i=0; i<arrayGame.length; i++){
- fetch ('http://www.omdbapi.com/?i='+arrayGame[i]+'&apikey=19e49d').then((data) =>{
-   return data.json();
- }).then ((dataAsJson) => {
-  //peliculas[i]= dataAsJson;
-  showPeliculas(dataAsJson);
+for ( i in arrayMoviesSelected) {
+  fetch('http://www.omdbapi.com/?i=' + arrayMoviesSelected[i] + '&apikey=19e49d').then((data) => {
+    return data.json();
+  }).then((dataAsJson) => {
+    //movies[i]= dataAsJson;
+    showmovies(dataAsJson);
 
 
- });
- }
- //console.log(peliculas);
+  });
+}
+ //console.log(movies);
